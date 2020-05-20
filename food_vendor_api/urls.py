ad7383583cb79a991ee903fb128f7946f90d355d
Your Name <onuhchukwuma12@gmail.com>
2020-05-20 08:05:32 +0100

    modified root urls

--- food_vendor_api/urls.py
+++ food_vendor_api/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from allauth.account.views import confirm_email
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
 from rest_framework_swagger.views import get_swagger_view
@@ -14,6 +15,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    
+    path('api/v1/account/', include('allauth.urls')),
+    path('api/v1/accounts-rest/registration/account-confirm-email/<int:pk>/', confirm_email, name='account_confirm_email'),
+
     path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
     path('', schema_view),
 ]
