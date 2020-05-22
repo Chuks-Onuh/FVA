d9c2223d04d61b95f355b37fc3b00aed4e36fc16
Your Name <onuhchukwuma12@gmail.com>
2020-05-22 23:46:24 +0100

    Modified settings.py

--- food_vendor_api/settings.py
+++ food_vendor_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -161,6 +161,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 AUTH_USER_MODEL = 'api.AuthUser'
 
