15e40465174b51329a5b1087c6acb78e2237ef79
Your Name <onuhchukwuma12@gmail.com>
2020-05-25 18:57:05 +0100

    added migrations

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0011_update_proxy_permissions'),
-        ('Vendor', '__first__'),
+        ('api', '__first__'),
     ]
 
     operations = [
