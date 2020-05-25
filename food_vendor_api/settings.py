adc05e682bbcd84031b1ca2fc7a5e5a9c3f8e565
Your Name <onuhchukwuma12@gmail.com>
2020-05-25 16:43:03 +0100

    added site_id in settings.py

--- food_vendor_api/settings.py
+++ food_vendor_api/settings.py
@@ -176,6 +176,7 @@ ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = '/?verification=1'
 ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = '/?verification=1'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-SITE_ID = 1
+
+SITE_ID=1
 
 django_heroku.settings(locals())
