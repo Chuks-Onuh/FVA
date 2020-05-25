f616bbb516af0b69772e7dbbc49c2ad6c6eeb416
Your Name <onuhchukwuma12@gmail.com>
2020-05-25 19:46:27 +0100

    added migration

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -107,8 +107,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('message', models.TextField()),
                 ('dateTimeCreated', models.DateTimeField(auto_now_add=True)),
-                ('messageStatus', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.MessageStatus')),
                 ('orderId', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.Order')),
+                ('messageStatus', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.MessageStatus')),
                 ('subjectUser', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.Vendor')),
             ],
             options={
