8071f956e7231fac1ba5f3c3a997101112ff18b5
Your Name <onuhchukwuma12@gmail.com>
2020-05-26 07:38:47 +0100

    added migrations

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -94,8 +94,8 @@ class Migration(migrations.Migration):
                 ('orderStatus', models.IntegerField(choices=[(1, 'Pendind'), (2, 'Delivered'), (3, 'Cancelled')])),
                 ('dateAndTimeOfOrder', models.DateTimeField(auto_now_add=True)),
                 ('customer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.Customer')),
-                ('menuId', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.Menu')),
                 ('vendorId', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.Vendor')),
+                ('menu', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.Menu')),
             ],
             options={
                 'ordering': ('-pk',),
